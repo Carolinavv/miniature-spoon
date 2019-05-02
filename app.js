5842b1e4c94e1f49d59c8a03ba7ee6b09b57ab25
GTR487 <gunther.molina.487@gmail.com>
2019-05-02 17:28:09 -0300

    fuerzas aplicadas

--- app.js
+++ app.js
@@ -1,27 +1,39 @@
 window.addEventListener('DOMContentLoaded', ()=>{
     var fps = 15;
     var cubo = document.getElementById("cub");
-    var i = 0;
-    var Hmax = window.innerHeight;
-    var Hmin = 0;
-    var Wmax = window.innerWidth;
-    var Wmin = 0;
-    var pos = [Math.random() * (Wmax - Wmin) + Wmin, Math.random() * (Hmax - Hmin) + Hmin];
-    cubo.style.left = pos[0];
-    cubo.style.top = pos[1];
-    var left = pos[0] + (i *10);
-    var top = pos[1] + (i *10);
     var timer = window.setInterval(()=>{
-        i++;
-        if(left < 0) left = Wmax;
-        else left = left[0] + (i *10);
-        if(left > Wmax) left = 0;
-        else left = left[0] + (i *10);
-        if(top < 0) top = Hmax;
-        else top = top[1] + (i *10);
-        if(top < Hmax) top = 0;
-        else top = top[1] + (i *10);
-        cubo.style.left = left + "px" ;
-        cubo.style.top =  top + "px";
+
     }, 1000/fps);
 });
+
+class cuerpo{
+    constructor(masa, size){
+        this.masa = masa;
+        this.size = size;
+        this.pos  = {
+                        X: 0,
+                        Y: 0
+                    };
+        this.vel  = {
+                        X: 0,
+                        Y: 0
+                    };
+        this.acc  = {
+                        X: 0,
+                        Y: 0
+                    };
+    }
+
+    mover(){
+        var fuerzas = 0;
+        arguments.array.forEach(fuerza => {
+            fuerzas += element;
+        });
+    }
+}
+
+posicion CAMBIA CON velocidad
+
+F= ma
+
+fuerza sobre cuerpo (masa, aceleracion)
